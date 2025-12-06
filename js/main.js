ae6109f328d24f465ca6a013db9cf0bb575027b4
hitotoki-1006 <roadmark.w@gmail.com>
2025-12-07 08:20:48 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@ $(window).on('resize', function () {
   // ハンバーガーメニューを閉じる
   hamburger.removeClass('hamburger-menu-active');
 });
+
+//カルーセル
+$('.cover-slick').slick({
+  autoplay: true, // 自動再生
+  autoplaySpeed: 3000, // 自動再生の速さ、単位はミリ秒
+  infinite: true,
+  dots: true,
+  arrows: true,
+  fade: true
+});
